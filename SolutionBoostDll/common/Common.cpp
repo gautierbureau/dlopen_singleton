628cb913eeae3814385a40c6e250a4f7d2508c96
Gautier Bureau <gautier.bureau@gmail.com>
2021-01-04 13:53:06 +0100

    test util.

--- SolutionBoostDll/common/Common.cpp
+++ SolutionBoostDll/common/Common.cpp
@@ -6,8 +6,11 @@
 
 #include "Common.hpp"
 #include "Log.hpp"
+#include "Util.hpp"
 
 void Common::common() {
   Trace("Log from common");
   std::cout << "common" << std::endl;
+  Util u;
+  u.util();
 }
